c820edda5e20119d1e52f0d055faa64495c7029e
Maged <mostafa.maged@intellaworld.com>
2025-10-30 09:55:41 +0300

    push docker image

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.post('/data', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
